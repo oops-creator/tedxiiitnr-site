4ec193ed352c09f595c5fbedc453fe52109228ea
oops-creator <still.shivam@gmail.com>
2021-01-09 18:06:56 +0530

    Add welcome screen

---
+++ src/components/Welcome/Welcome.js
@@ -0,0 +1,27 @@
+import './Welcome.css';
+import React from 'react';
+
+
+function Welcome() {
+    return (
+        <React.Fragment>
+            <div class="welcome-area" id="welcome">
+
+                <div class="header-text">
+                    <div class="container">
+                        <div class="row">
+                            <div class="offset-xl-3 col-xl-6 offset-lg-2 col-lg-8 col-md-12 col-sm-12">
+                                <h1>Welcome here something something</h1>
+
+                                <h1 class="tedxtitle"><span class="boldspan"><strong>TED<sup>x</sup></strong></span>IIITNayaRaipur<span id='bouncydot'></span></h1>
+            </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+      </React.Fragment>
+    
+  );
+}
+
+export default Welcome;
